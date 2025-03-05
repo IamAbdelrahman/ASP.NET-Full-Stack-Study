5679823dd1fe56c3822e2bb3a744e3cc9fffbfe5
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-03-05 21:17:34 +0200

    Math Operation

---
+++ C#/Training/SimpleMathEvaluator/MathOperation.cs
@@ -0,0 +1,27 @@
+﻿/******************************************************************************
+ * Copyright (C) 2025 by Abdelrahman Kamal - CSharpFundamentalsCourse by M.ElMahdy
+ *****************************************************************************/
+
+/*****************************************************************************
+ * FILE DESCRIPTION
+ * ----------------------------------------------------------------------------
+ *  @file: MathOperation.cs
+ *  @brief This file includes an enumeration about all supported math operations
+ *
+ *****************************************************************************/
+namespace CSharpLabs;
+
+public enum MathOperation
+{
+  None,
+  Addition,
+  Subtraction,
+  Multiplication,
+  Division,
+  Power,
+  Modulus,
+  Sin,
+  Cos,
+  Tan
+
+}
