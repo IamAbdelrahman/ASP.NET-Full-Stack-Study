422ed1d02933eda7341e9f6ba38663edf0dab544
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-03-05 21:17:51 +0200

    Math Expression

---
+++ C#/Training/SimpleMathEvaluator/MathExpression.cs
@@ -0,0 +1,22 @@
+﻿/******************************************************************************
+ * Copyright (C) 2025 by Abdelrahman Kamal - CSharpFundamentalsCourse by M.ElMahdy
+ *****************************************************************************/
+
+/*****************************************************************************
+ * FILE DESCRIPTION
+ * ----------------------------------------------------------------------------
+ *  @file: MathExpression.cs
+ *  @brief This file includes the expression's elements
+ * as it includes operands and the type of operation
+ *
+ *****************************************************************************/
+
+namespace CSharpLabs;
+
+public class MathExpression
+{
+  public double LeftSideOperand { get; set; }
+  public double RightSideOperand { get; set; }
+  public MathOperation Operation { get; set; }
+
+}
