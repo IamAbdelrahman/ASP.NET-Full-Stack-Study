6ad912d14ab339efddf9d19dc15110e6660f155b
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-03-05 22:58:59 +0200

    Generate a Random Password

---
+++ C#/Training/PasswordManager/GeneratePassword.cs
@@ -0,0 +1,31 @@
+﻿/******************************************************************************
+ * Copyright (C) 2025 by Abdelrahman Kamal - CSharpFundamentalsCourse by M.ElMahdy
+ *****************************************************************************/
+
+/*****************************************************************************
+ * FILE DESCRIPTION
+ * ----------------------------------------------------------------------------
+ *  @file: GeneratePassword.cs
+ *  @brief This file shows how to generate a password randomly using a string
+ * contains all characters and digits
+ *****************************************************************************/
+
+namespace CSharpLabs.PassWordManager;
+using System.Text;
+
+public class GeneratePassword
+{
+  public StringBuilder sb = new StringBuilder();
+  public string GetRandomPassword()
+  {
+    int size = 8;
+    string allChars = EncryptionUtility._orderedChars;
+    Random rand = new Random();
+    while (size > 0)
+    {
+      sb.Append(allChars[rand.Next(allChars.Length)]);
+      size--;
+    }
+    return sb.ToString();
+  }
+}
