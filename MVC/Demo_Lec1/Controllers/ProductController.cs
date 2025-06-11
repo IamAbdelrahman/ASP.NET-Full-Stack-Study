4afe66dce1b5f4d422eca5c19bf793720ff0c653
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-06-11 18:41:00 +0300

    Lecture_1_Task

---
+++ MVC/Demo_Lec1/Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿using Demo_Lec1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Demo_Lec1.Models;
+namespace Demo_Lec1.Controllers
+{
+    public class ProductController : Controller
+    {
+        /*
+         Methods cannot be private or static
+         Methods cannot be overloaded except (in some cases)
+         Action Methods can return:-
+         String => ContentResult
+         View "Html" => ViewResult
+         JSON => JSONResul
+         JavaScript => JavascriptResult
+         File => FileResult
+         Notfound => NotfoundResult
+        - 
+         */
+        public ContentResult ShowMessage()
+        {
+            ContentResult result = new ContentResult();
+            result.Content = "Local Message";
+            return result;
+        }
+        public ContentResult ShowMssage2()
+        {
+            return Content("Local Message2");
+        }
+
+        public ViewResult ShowView()
+        {
+            ViewResult result = new ViewResult();
+            result.ViewName = "View";
+            return result;
+        }
+        public ViewResult ShowView2()
+        {
+            return View("View");
+        }
+        public JsonResult ShowJson()
+        {
+            JsonResult result = new JsonResult(new { ID = 1, Name = "AK" });
+            return result;
+        }
+        public JsonResult ShowJson2()
+        {
+            return Json(new { id = 1, name = "hossam" });
+        }
+        public IActionResult ShowMix(int id)
+        {
+            if (id % 2 == 0)
+            {
+                ViewResult result = new ViewResult();
+                result.ViewName = "View";
+                return result;
+            }
+            else
+            {
+                ViewResult result = new ViewResult();
+                result.ViewName = "View";
+                return result;
+            }
+        }
+        /*
+         To send parameters to the methods we can do the following:-
+        /Product/ShowMix/1 >> with action parameter id only
+        /Product/ShowMix?id=1 >> with any action parameter
+
+        -- In case of more than one parameter
+        /Product/ShowMix?id=1&age=44
+        /Product/ShowMix/1&age=22
+         */
+
+        /*- Handle Product Operations */
+        ProductRepo productRepo = new ProductRepo();
+        public IActionResult ProductDetails (int id)
+        {
+            Product modal = productRepo.GetProductById(id);
+            return View("ProductDetails", modal);
+        }
+        public IActionResult AllProductDetails()
+        {
+            List<Product> productsModel = productRepo.GetProducts();
+            return View("ShowAllProducts", productsModel);
+        }
+    }
+}
---
+++ MVC/Demo_Lec1/Models/ProductRepo.cs
@@ -0,0 +1,27 @@
+﻿namespace Demo_Lec1.Models
+{
+    public class ProductRepo
+    {
+        public List<Product> Products = new List<Product>();
+        public List<Product> LoadProducts()
+        {
+            return new List<Product>()
+            {
+                new Product() { Id = 1, Name = "Iphone1", Description = "Mobile1", Image="1.png", Price = 1000 },
+                new Product() { Id = 2, Name = "Iphone2", Description = "Mobile2", Image="2.png", Price = 2000 },
+                new Product() { Id = 3, Name = "Iphone3", Description = "Mobile3", Image="3.png", Price = 3000 },
+                new Product() { Id = 4, Name = "Iphone4", Description = "Mobile4", Image="4.png", Price = 4000 }
+            };
+        }
+        public List<Product> GetProducts()
+        {
+            return LoadProducts();
+        }
+        public Product GetProductById(int id)
+        {
+            var products = GetProducts();
+            var result = products.Find(x => x.Id == id);
+            return result;
+        }
+    }
+}
