dd0385552b7c904e35fc84eb095f2d5775624040
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-09-28 15:09:33 +0300

    delete unecessary files

---
+++ JavaScript/02_ElZero/Assignments/DataTypes-Variables/main.js
@@ -0,0 +1,23 @@
+// Assignment 1
+let numOne = 10, numTwo = 20;
+console.log(numOne +""+ numTwo);
+console.log(typeof(numOne.toString() + numTwo.toString()));
+console.log(`${numOne}` + `${numTwo}`);
+console.log(typeof(`${numOne}` + `${numTwo}`));
+console.log(`${numOne}
+${numTwo}`);
+console.log(numOne + "\n" + numTwo);
+
+// Assignment 2
+var elzero = { "innerHTML": "object" };
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+// Assignment 3
+console.log("I'm In\n\\\\\nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
+
+// Assignment 4
+let a = 21;
+let b = 20;
+
+console.log(`${a}_${b}${a}_${b}${a}_${b}${a}_${a}`); // _21_2021_2021_2021_20_
