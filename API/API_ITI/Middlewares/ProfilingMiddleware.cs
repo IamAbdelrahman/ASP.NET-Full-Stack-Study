d1b5ddce602c03a3b9de1144234971f9b5caa8aa
IamAbdelrahman <abdelruhman.m.kamal@gmail.com>
2025-11-02 13:04:41 +0200

    add a middleware to measure the response's time, and a rate-limiting one

---
+++ API/API_ITI/Middlewares/ProfilingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Demos.Middlewares
+{
+    public class ProfilingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ProfilingMiddleware> _logger;
+        public ProfilingMiddleware(RequestDelegate next, ILogger<ProfilingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+        
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var stopwatch = new Stopwatch();
+            await _next(context);
+            stopwatch.Stop();
+            _logger.LogInformation($"Request [{context.Request.Path}]executed in {stopwatch.ElapsedMilliseconds} ms");
+        }
+    }
+}
---
+++ API/API_ITI/Middlewares/RateLimitingMiddleware.cs
@@ -0,0 +1,36 @@
+﻿namespace Demos.Middlewares
+{
+    public class RateLimitingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private static int _count = 0;
+        private static DateTime _lastRequestDate = DateTime.Now; 
+        public RateLimitingMiddleware(RequestDelegate next)
+        {
+            _next=next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            _count++;
+            if (DateTime.Now.Subtract(_lastRequestDate).TotalSeconds > 10)
+            {
+                _count = 1;
+                _lastRequestDate = DateTime.Now;
+                await _next(context);
+            }
+            else
+                {
+                if (_count <= 5)
+                {
+                    _lastRequestDate = DateTime.Now;
+                    await _next(context);
+                }
+                else
+                {
+                    context.Response.StatusCode = 429; // Too Many Requests
+                    await context.Response.WriteAsync("Rate limit exceeded. Try again later.");
+                }
+            }
+        }
+    }
+}
